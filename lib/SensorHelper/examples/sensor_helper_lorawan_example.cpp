706351800b80c1e93a9159c18aa616e2bf33dc36
Kalina <kalina.knight77@gmail.com>
2021-10-06 20:36:17 +1100

    fixed code formatting

--- lib/SensorHelper/examples/sensor_helper_lorawan_example.cpp
+++ lib/SensorHelper/examples/sensor_helper_lorawan_example.cpp
@@ -17,10 +17,10 @@
 #include <LoRaWan-RAK4630.h> // Click to get library: https://platformio.org/lib/show/6601/SX126x-Arduino
 
 #include "LoRaWAN_functs.h" /**< Go here to provide the OTAA keys & change the LoRaWAN settings. */
-#include "Logging.h" /**< Go here to change the logging level for the entire application. */
-#include "OTAA_keys.h" /**< Go here to set the OTAA keys (See LoRaWAN_functs README). */
-#include "PortSchema.h" /**< Go here to see existing and define new sensor/port schemas. */
-#include "SensorHelper.h" /**< Go here to add code for new additional sensors. */
+#include "Logging.h"        /**< Go here to change the logging level for the entire application. */
+#include "OTAA_keys.h"      /**< Go here to set the OTAA keys (See LoRaWAN_functs README). */
+#include "PortSchema.h"     /**< Go here to see existing and define new sensor/port schemas. */
+#include "SensorHelper.h"   /**< Go here to add code for new additional sensors. */
 
 // PORT/SENSOR SELECTION
 // The chosen port determines the sensor data included in the payload - see
@@ -32,11 +32,9 @@ portSchema payload_port = PORT3;
 const int sensor_reading_interval = 30000;
 
 // PAYLOAD ENCODING
-uint8_t payload_buffer[PAYLOAD_BUFFER_SIZE] =
-    {}; /**< Buffer that payload data is placed in. */
-lmh_app_data_t lorawan_payload = {
-    payload_buffer, 0, 0, 0, 0}; /**< Struct that passes the payload buffer and
-                                    relevant params for a LoRaWAN frame. */
+uint8_t payload_buffer[PAYLOAD_BUFFER_SIZE] = {};                /**< Buffer that payload data is placed in. */
+lmh_app_data_t lorawan_payload = { payload_buffer, 0, 0, 0, 0 }; /**< Struct that passes the payload buffer and
+                                                                    relevant params for a LoRaWAN frame. */
 // forward declaration
 void fillPayload(void);
 
@@ -44,43 +42,44 @@ void fillPayload(void);
  * @brief Setup code runs once on reset/startup.
  */
 void setup() {
-  // initialise the logging module - function does nothing if APP_LOG_LEVEL in
-  // Logging.h = NONE
-  initLogging();
-  log(LOG_LEVEL::INFO, "\n========================================"
-                       "\nWelcome to Sensor Helper LoRaWAN Example"
-                       "\n========================================");
+    // initialise the logging module - function does nothing if APP_LOG_LEVEL in
+    // Logging.h = NONE
+    initLogging();
+    log(LOG_LEVEL::INFO,
+        "\n========================================"
+        "\nWelcome to Sensor Helper LoRaWAN Example"
+        "\n========================================");
 
-  // Init sensors according to payload_port selected
-  if (!initSensors(&payload_port, true, false)) {
-    return;
-  }
+    // Init sensors according to payload_port selected
+    if (!initSensors(&payload_port, true, false)) {
+        return;
+    }
 
-  // Init LoRaWAN
-  if (!initLoRaWAN(OTAA_KEY_APP_EUI, OTAA_KEY_DEV_EUI, OTAA_KEY_APP_KEY)) {
-    return;
-  }
+    // Init LoRaWAN
+    if (!initLoRaWAN(OTAA_KEY_APP_EUI, OTAA_KEY_DEV_EUI, OTAA_KEY_APP_KEY)) {
+        return;
+    }
 
-  // Attempt to join the network
-  startLoRaWANJoinProcedure();
+    // Attempt to join the network
+    startLoRaWANJoinProcedure();
 }
 
 /**
  * @brief Loop code runs repeated after setup().
  */
 void loop() {
-  // every sensor_reading_interval ms check if connected and then send sensor
-  // payload
-  delay(sensor_reading_interval);
-  if (isLoRaWANConnected()) {
-    log(LOG_LEVEL::DEBUG, "Send payload");
-    // fill lora data buffer
-    fillPayload();
-    // send data
-    sendLoRaWANFrame(&lorawan_payload);
-  } else {
-    log(LOG_LEVEL::DEBUG, "LoRaWAN not connected. Try again later.");
-  }
+    // every sensor_reading_interval ms check if connected and then send sensor
+    // payload
+    delay(sensor_reading_interval);
+    if (isLoRaWANConnected()) {
+        log(LOG_LEVEL::DEBUG, "Send payload");
+        // fill lora data buffer
+        fillPayload();
+        // send data
+        sendLoRaWANFrame(&lorawan_payload);
+    } else {
+        log(LOG_LEVEL::DEBUG, "LoRaWAN not connected. Try again later.");
+    }
 }
 
 /**
@@ -89,24 +88,21 @@ void loop() {
  * PortSchema.h.
  */
 void fillPayload(void) {
-  // get the sensor data
-  sensorData sensor_data = {};
-  sensor_data = getSensorData(&payload_port);
+    // get the sensor data
+    sensorData sensor_data = {};
+    sensor_data = getSensorData(&payload_port);
 
-  log(LOG_LEVEL::INFO,
-      "b: %.2f %% | t: %.2f C | h: %.2f %% | p: %lu Pa | g: %lu | l: %.5f, "
-      "%.5f",
-      sensor_data.battery_mv.value, sensor_data.temperature.value,
-      sensor_data.humidity.value, sensor_data.pressure.value,
-      sensor_data.gas_resist.value, sensor_data.location.latitude,
-      sensor_data.location.longitude);
+    log(LOG_LEVEL::INFO,
+        "b: %.2f %% | t: %.2f C | h: %.2f %% | p: %lu Pa | g: %lu | l: %.5f, "
+        "%.5f",
+        sensor_data.battery_mv.value, sensor_data.temperature.value, sensor_data.humidity.value, sensor_data.pressure.value,
+        sensor_data.gas_resist.value, sensor_data.location.latitude, sensor_data.location.longitude);
 
-  // clear the buffer
-  memset(payload_buffer, 0, sizeof(payload_buffer));
-  lorawan_payload.buffsize = 0;
-  lorawan_payload.port = payload_port.port_number;
+    // clear the buffer
+    memset(payload_buffer, 0, sizeof(payload_buffer));
+    lorawan_payload.buffsize = 0;
+    lorawan_payload.port = payload_port.port_number;
 
-  // encode the sensor data to lorawan_payload
-  lorawan_payload.buffsize =
-      payload_port.encodeSensorDataToPayload(&sensor_data, payload_buffer);
+    // encode the sensor data to lorawan_payload
+    lorawan_payload.buffsize = payload_port.encodeSensorDataToPayload(&sensor_data, payload_buffer);
 }
